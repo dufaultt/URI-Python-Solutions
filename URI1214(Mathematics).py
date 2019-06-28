833e9a3336977d330c34036cd06bc98d4033c2ec
Tristan Dufault <32806910+dufaultt@users.noreply.github.com>
2019-06-28 15:39:08 -0400

    Create URI1214(Mathematics).py

---
+++ URI1214(Mathematics).py
@@ -0,0 +1,29 @@
+##Tristan Dufault 2019/06/28
+
+iterate = int(input())
+
+while(iterate > 0):
+
+    iterate -= 1
+    inp = input()
+    inpl = inp.split(" ")
+    sum = 0
+    count = 0
+    N = int(inpl[0])
+    N1 = N
+
+    while(N1 > 0):
+        sum += int(inpl[N1])
+        N1 -= 1
+
+    avg = sum/N
+    N1 = N
+
+    while(N1 > 0):
+        if(int(inpl[N1]) > avg):
+            count+=1
+        N1-=1
+
+    perc = (count/N)*100
+
+    print('{:.3f}%'.format(perc))
