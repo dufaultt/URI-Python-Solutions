fb2c613667f9961adb26d3e0870a9d98ef1c8f2a
Tristan Dufault <32806910+dufaultt@users.noreply.github.com>
2019-07-11 11:32:31 -0400

    Update URI1030(Ad-Hoc).py

--- URI1030(Ad-Hoc).py
+++ URI1030(Ad-Hoc).py
@@ -1,10 +1,14 @@
-#Tristan Dufault -work in progress
+#Tristan Dufault -Work in progress
 #Problem link: https://www.urionlinejudge.com.br/judge/en/problems/view/1030
 iterate = int(input())
 num = 1
 
 while(iterate > 0):
     ans = 0
+    flag = 0
+    i = 0
+    cyc = 0
+    deaths = 0
     inp = input()
     inpl = inp.split(" ")
 
@@ -16,7 +20,31 @@ while(iterate > 0):
         circle[i] = i+1
     #end for
 
-    print(circle)
+    while(flag == 0):
+
+        i+=1
+        if(i == len(circle)):
+            i = 0
+        #end if
+
+        if(circle[i] != 0):
+            cyc +=1
+        #end if
+        if(cyc == step):
+            circle[i] = 0
+            deaths+=1
+            cyc = 0
+        #end if
+
+        if(deaths == (total-1)):
+            flag = 1
+        #end if
+    #end while
+    for i in range(len(circle)):
+        if(circle[i] != 0):
+            ans = i+1
+        #end if
+    #end for
     print('Case {:d}: {:d}'.format(num, ans))
     num+=1
     iterate-=1
