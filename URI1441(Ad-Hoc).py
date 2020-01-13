4a31aa77584d7ea0f5ff622f0e5c9604f08e8f66
Tristan Dufault <32806910+dufaultt@users.noreply.github.com>
2020-01-13 11:24:31 -0500

    Create URI1441(Ad-Hoc).py

---
+++ URI1441(Ad-Hoc).py
@@ -0,0 +1,26 @@
+#Tristan Dufault 2020-01-13
+#Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1441
+
+n = int(input())
+
+
+while (n != 0):
+    ans = 1
+    n1 = 0
+    while(n != 1):
+
+        if(n > ans):
+            ans = n
+        #end if
+
+        if(n % 2) == 0:
+            n1 = int(n/2)
+        else:
+            n1 = (n*3)+1
+        #end if
+        n = n1
+
+    #end while
+    print(ans)
+    n = int(input())
+#end while
