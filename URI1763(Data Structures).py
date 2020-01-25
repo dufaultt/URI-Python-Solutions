bbea00e47c62891500ebcd967968547bf06c5bd8
Tristan Dufault <32806910+dufaultt@users.noreply.github.com>
2020-01-24 22:53:28 -0500

    Update URI1763(Data Structures).py

--- URI1763(Data Structures).py
+++ URI1763(Data Structures).py
@@ -1,5 +1,5 @@
 #Tristan Dufault 2020-01-23
-#Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1763
+#Problem Link: https://www.urionlinejudge.com.br/judge/en/problems/view/1763 
 
 countrlist = ['brasil','alemanha','austria','coreia','espanha','grecia','estados-unidos','inglaterra','australia','portugal','suecia','turquia','argentina','chile','mexico','antardida','canada','irlanda','belgica','italia','libia','siria','marrocos','japao']
 greetlist = ['Feliz Natal!','Frohliche Weihnachten!','Frohe Weihnacht!','Chuk Sung Tan!','Feliz Navidad!','Kala Christougena!','Merry Christmas!','Merry Christmas!','Merry Christmas!','Feliz Natal!','God Jul!','Mutlu Noeller','Feliz Navidad!','Feliz Navidad!','Feliz Navidad!','Merry Christmas!','Merry Christmas!','Nollaig Shona Dhuit!','Zalig Kerstfeest!','Buon Natale!','Buon Natale!','Milad Mubarak!','Milad Mubarak!','Merii Kurisumasu!']
