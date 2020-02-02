b5cd3b2de176061327372834a51a368af1329113
Tristan Dufault <32806910+dufaultt@users.noreply.github.com>
2020-02-02 17:49:22 -0500

    Update URI1763(Data Structures).py

--- URI1763(Data Structures).py
+++ URI1763(Data Structures).py
@@ -4,7 +4,7 @@
 countrlist = ['brasil','alemanha','austria','coreia','espanha','grecia','estados-unidos','inglaterra','australia','portugal','suecia','turquia','argentina','chile','mexico','antardida','canada','irlanda','belgica','italia','libia','siria','marrocos','japao']
 greetlist = ['Feliz Natal!','Frohliche Weihnachten!','Frohe Weihnacht!','Chuk Sung Tan!','Feliz Navidad!','Kala Christougena!','Merry Christmas!','Merry Christmas!','Merry Christmas!','Feliz Natal!','God Jul!','Mutlu Noeller','Feliz Navidad!','Feliz Navidad!','Feliz Navidad!','Merry Christmas!','Merry Christmas!','Nollaig Shona Dhuit!','Zalig Kerstfeest!','Buon Natale!','Buon Natale!','Milad Mubarak!','Milad Mubarak!','Merii Kurisumasu!']
 
-lenc = len(countrlist)
+lenc = len(countrlist) 
 leng = len(greetlist) 
  
 
